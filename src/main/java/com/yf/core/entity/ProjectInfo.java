297dc5a81c831ef342d676840095747acc26aad5
xdmdpz <xdmdpz@foxmail.com>
2017-12-01 19:29:21 +0800

    暂不使用h2数据库

--- src/main/java/com/yf/core/entity/ProjectInfo.java
+++ src/main/java/com/yf/core/entity/ProjectInfo.java
@@ -1,46 +1,47 @@
 package com.yf.core.entity;
 
 import com.yf.core.base.BaseEntity;
+
 import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
 @Table(name = "project_info", schema = "generater")
-public class ProjectInfo extends BaseEntity<String> implements Serializable{
-            //项目名称
+public class ProjectInfo extends BaseEntity<String> implements Serializable {
+    //项目名称
     private String project_name;
-            //项目描述
+    //项目描述
     private String project_desc;
-                	public  ProjectInfo (){
-		super();
-	}
-	@Id
-	@Column(name = "project_info_id", nullable = false, length = 32)
-	public String getId() {
-		return id;
-	}
 
-	public void setId(String id) {
-		this.id = id;
-	}
-    
-        public String getProject_name(){
-    	return this.project_name;
+    public ProjectInfo() {
+        super();
     }
-    public void setProject_name(String project_name){
-    	this.project_name = project_name;
+
+    @Id
+    @Column(name = "project_info_id", nullable = false, length = 32)
+    public String getId() {
+        return id;
     }
-    
-        public String getProject_desc(){
-    	return this.project_desc;
+
+    public void setId(String id) {
+        this.id = id;
     }
-    public void setProject_desc(String project_desc){
-    	this.project_desc = project_desc;
+
+    public String getProject_name() {
+        return this.project_name;
     }
-    
-    
-    
-    
+
+    public void setProject_name(String project_name) {
+        this.project_name = project_name;
+    }
+
+    public String getProject_desc() {
+        return this.project_desc;
+    }
+
+    public void setProject_desc(String project_desc) {
+        this.project_desc = project_desc;
+    }
 
 
 }
